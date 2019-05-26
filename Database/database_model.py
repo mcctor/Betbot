4198dadfdf365aaf86fb811ad6048b545c8f6cb0
mcctor <markvictormwanga@gmail.com>
2019-05-26 21:07:56 +0300

    Added a rough database to store bet history data.

---
+++ Database/database_model.py
@@ -0,0 +1,28 @@
+import sqlite3
+
+from sqlalchemy import create_engine
+from sqlalchemy import Column, Integer, String, Float
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+engine = create_engine('sqlite:///sqlite_db', echo=False)
+
+Session = sessionmaker(bind=engine)
+db_session = Session()
+
+Base = declarative_base()
+
+
+class BetHistory(Base):
+    __tablename__ = 'bet_history'
+
+    bet_href = Column(String, primary_key=True)
+    won = Column(Integer)
+    bet_odds = Column(Float)
+    profit = Column(Float)
+
+    def __repr__(self):
+        return "<Bet(bet_href='{}', won='{}', profit='{}')>".format(self.bet_href, self.won, self.profit)
+
+
+Base.metadata.create_all(engine)
