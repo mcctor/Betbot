7e1b3c80e2704930fe86a530ae1c4374638fd899
mcctor <markvictormwanga@gmail.com>
2019-05-26 21:09:28 +0300

    Edited various comments.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from Odi.odibot import Odibot
 
 if __name__ == '__main__':
     # Instantiate Odibot Class by giving phone and password arguments.
-    bet_bot = Odibot(phone_number="0740779569", password="Abacadabra01", stake_figure=50)
+    bet_bot = Odibot(phone_number="0740779569", password="Abacadabra01", stake_figure=30)
 
     # Execute the first basis of the betting strategy.
     bet_bot.run_first_basis()
