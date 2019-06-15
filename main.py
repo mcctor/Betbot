8af03d15f0c4bb7eca939e48856fd2b40c32444c
mcctor <markvictormwanga@gmail.com>
2019-06-15 11:28:38 +0300

    Added commandline parser to `main.py`.

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
+import argparse
+
 from Odi.odibot import Odibot
 
 if __name__ == '__main__':
+
+    parse = argparse.ArgumentParser(
+        description='A script meant for automated betting with https://odibets.com. The strategy it'
+                    ' uses is highlighted in the README.md.'
+    )
+
+    parse.add_argument('phone_number', help='Your phone number as registered with https://odibet.com')
+    parse.add_argument('password', help='Your odibets password.')
+    parse.add_argument('stake', type=int, help='The maximum stake figure this script will ever work with.')
+
+    # parse the two arguments.
+    args = parse.parse_args()
+
     # Instantiate Odibot Class by giving phone and password arguments.
-    bet_bot = Odibot(phone_number="0740779569", password="Abacadabra01", stake_figure=30)
+    bet_bot = Odibot(phone_number=args.phone_number, password=args.password, stake_figure=args.stake)
 
     # Execute the first basis of the betting strategy.
     bet_bot.run_first_basis()
